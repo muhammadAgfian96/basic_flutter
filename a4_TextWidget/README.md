60020596fa4052b44d1c0a7bb459c1e67d08bfb8
boy <zaheengate@gmail.com>
2020-09-22 11:22:48 +0700

    a4 learn text and container

---
+++ a4_TextWidget/README.md
@@ -0,0 +1,5 @@
+# a4_TextWidget
+
+membahas tentang container dan text widget, dan propertinya
+
+<img src="./a4.gif" width="200" height="400" />
---
+++ a4_TextWidget/android/app/src/main/kotlin/com/example/a4_TextWidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a4_TextWidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
