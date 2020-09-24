996e7387a164040bea50b569fa646c6a396cda2d
boy <zaheengate@gmail.com>
2020-09-25 02:20:49 +0700

    learning TextStyle

---
+++ a10_textStyle/android/app/src/main/kotlin/com/example/a10_textStyle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a10_textStyle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ a10_textStyle/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan TextStyle'),
+        ),
+        body: Center(
+          child: Text(
+            'Uzumaki Agfian Namikaze!',
+            style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'Raleway',
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationThickness: 4,
+                decorationStyle: TextDecorationStyle.wavy,
+                wordSpacing: 0.5),
+          ),
+        ),
+      ),
+    );
+  }
+}
