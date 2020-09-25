bf7845a33925f896b31351218e03e688cc9492de
boy <zaheengate@gmail.com>
2020-09-25 13:26:22 +0700

    learn flexible widget

--- a11_animated_container/lib/main.dart
+++ a11_animated_container/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
---
+++ a12_FlexibleWidget/android/app/src/main/kotlin/com/example/a12_FlexibleWidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a12_FlexibleWidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
