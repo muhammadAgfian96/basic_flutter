7fe279c04c9bee54d302f6904ad615d4082de1ee
boy <zaheengate@gmail.com>
2020-09-25 17:34:18 +0700

    learn image asset

---
+++ a14_image_widget/android/app/src/main/kotlin/com/example/a14_image_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a14_image_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ a14_image_widget/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  String alamat =
+      'https://vignette.wikia.nocookie.net/naruto/images/d/dd/Naruto_Uzumaki%21%21.png/revision/latest/scale-to-width-down/1000?cb=20161013233552';
+  String alamat1 = 'assets/signs.svg';
+  String alamat2 = 'assets/images-icon.png';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image Widget'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(16),
+                color: Colors.black,
+                width: 300,
+                height: 200,
+                padding: EdgeInsets.all(3),
+                child: Image(
+                  image: NetworkImage(alamat),
+                  fit: BoxFit.contain,
+                  repeat: ImageRepeat.repeatY,
+                ),
+              ),
+              Spacer(),
+              Container(
+                margin: EdgeInsets.all(16),
+                color: Colors.black,
+                width: 300,
+                height: 200,
+                padding: EdgeInsets.all(3),
+                child: Image(
+                  image: AssetImage(alamat2),
+                  fit: BoxFit.contain,
+                  repeat: ImageRepeat.repeatY,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
