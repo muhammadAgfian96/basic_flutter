21bd0cacb0f6708591d33f704325724c735cad31
boy <zaheengate@gmail.com>
2020-09-27 00:35:18 +0700

    learn hero clipRRect

---
+++ a25_hero_clipRrect/android/app/src/main/kotlin/com/example/a25_hero_clipRrect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a25_hero_clipRrect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ a25_hero_clipRrect/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      appBar: AppBar(
+        title: Text('Latihan Hero Animation'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return SecondPage();
+          }));
+        },
+        child: Hero(
+          tag: 'pp',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Container(
+              width: 100,
+              height: 100,
+              child: Image(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/gambar.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[50],
+      appBar: AppBar(
+        title: Text('Latihan Hero Animation'),
+      ),
+      body: Center(
+        child: Hero(
+          tag: 'pp',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Image(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/gambar.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
